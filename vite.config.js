b2e4d866d9aee0493caa4b1da2cdbe4002867251
Amir Hawasly <93056805+amirishere101@users.noreply.github.com>
2025-05-12 17:55:59 -0400

    update1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Woodside-Deli-Website",
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0, // Ensure fonts are not inlined
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]", // Keep fonts in the assets folder
+      },
+    },
+  },
 });
