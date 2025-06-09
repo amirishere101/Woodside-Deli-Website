5a290a0f71ffeede9a904354bbbf068cf1ce0ffd
Amir Hawasly <93056805+amirishere101@users.noreply.github.com>
2025-06-08 22:03:27 -0400

    Update Home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import HeroImg from "../../assets/Images/unnamed (1) (1).png";
 import { Link } from "react-router-dom";
-import { BsLink } from "react-icons/bs";
 
 const Home = ({}) => {
   return (
